4aa865cd445d677f2505a1af10f1d45a4db52f4a
RyanMHsiao <109319635+RyanMHsiao@users.noreply.github.com>
2025-04-07 14:32:28 -0700

    Fix ordering issues with canvas and base-svg. Transformations are not properly working yet.

--- public/js/main.mjs
+++ public/js/main.mjs
@@ -34,8 +34,13 @@ function draw() {
 
 	camera.refreshTransform();
 
+	// Use CSS transforms to manipulate the background SVG
 	// TODO Move the logic for this call to camera for abstraction
-	// ctx.drawImage($("#background-map")[0], 0, 0);
+	$("#base-svg").css({
+		transform: `matrix(${camera.affine.transform.join(", ")})`,
+		"z-index": -50
+	});
+
 	// Feel free to experiment by adding some canvas draw calls here
 	// Here, I use the mercator object to convert my latitude and longitude
 	// into a format that ctx can understand
